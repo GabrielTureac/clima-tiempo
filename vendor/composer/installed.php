aa41356c621a5e64c8267b7cbf24d35aa8f2a374
GTureac <gabriel.tureac07@gmail.com>
2025-05-28 12:39:25 +0200

    Fix symfony auto-scripts for deployment

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '87db575e08dd8f60c238710289524ea8892dfaf1',
+        'reference' => '1ac01d55a843cb970847e78db8836ee540630dcc',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '87db575e08dd8f60c238710289524ea8892dfaf1',
+            'reference' => '1ac01d55a843cb970847e78db8836ee540630dcc',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
